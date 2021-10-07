9d68b17116e5d013299a14d9d128334126352f4d
Swathi <swathi.h@perfios.com>
2021-10-07 19:40:47 +0530

    MODIFIED: RegEx to restrict taking alphabet in the begining of the pincode

--- PinCodeValidator.js
+++ PinCodeValidator.js
@@ -2,6 +2,14 @@ let arguments = process.argv;
 arguments.splice(0, 2);
 
 // UC 1
+{
+    let pinCodeRegex = RegExp('[0-9]{6}');
+
+    arguments.forEach(element => {
+        console.log(element + " :", pinCodeRegex.test(element));
+    });
+}
+// UC 2
 let pinCodeRegex = RegExp('^[0-9]{6}');
 
 arguments.forEach(element => {
