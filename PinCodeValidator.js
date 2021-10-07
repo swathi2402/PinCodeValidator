9e03914914c3c539669e6bbbc3df9c884ff86607
Swathi <swathi.h@perfios.com>
2021-10-07 19:37:03 +0530

    ADDED: RegEx to validate pincode of six digits

---
+++ PinCodeValidator.js
@@ -0,0 +1,9 @@
+let arguments = process.argv;
+arguments.splice(0, 2);
+
+// UC 1
+let pinCodeRegex = RegExp('^[0-9]{6}');
+
+arguments.forEach(element => {
+    console.log(element + " :", pinCodeRegex.test(element));
+});
