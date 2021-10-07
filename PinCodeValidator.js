76673b5144861bd43d0ff86f488b7ce30bdae758
Swathi <swathi.h@perfios.com>
2021-10-07 19:42:30 +0530

    MODIFIED: RegEx to restrict taking alphabet in the end of the pincode

--- PinCodeValidator.js
+++ PinCodeValidator.js
@@ -10,7 +10,15 @@ arguments.splice(0, 2);
     });
 }
 // UC 2
-let pinCodeRegex = RegExp('^[0-9]{6}');
+{
+    let pinCodeRegex = RegExp('^[0-9]{6}');
+
+    arguments.forEach(element => {
+        console.log(element + " :", pinCodeRegex.test(element));
+    });
+}
+// UC 3
+let pinCodeRegex = RegExp('^[0-9]{6}$');
 
 arguments.forEach(element => {
     console.log(element + " :", pinCodeRegex.test(element));
