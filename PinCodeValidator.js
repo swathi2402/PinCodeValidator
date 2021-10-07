684733045d4cbb5d89375ca9bc1a86cf969fb680
Swathi <swathi.h@perfios.com>
2021-10-07 19:47:20 +0530

    MODIFIED: RegEx to allow a space in between

--- PinCodeValidator.js
+++ PinCodeValidator.js
@@ -4,7 +4,6 @@ arguments.splice(0, 2);
 // UC 1
 {
     let pinCodeRegex = RegExp('[0-9]{6}');
-
     arguments.forEach(element => {
         console.log(element + " :", pinCodeRegex.test(element));
     });
@@ -12,14 +11,21 @@ arguments.splice(0, 2);
 // UC 2
 {
     let pinCodeRegex = RegExp('^[0-9]{6}');
-
     arguments.forEach(element => {
         console.log(element + " :", pinCodeRegex.test(element));
     });
 }
-// UC 3
-let pinCodeRegex = RegExp('^[0-9]{6}$');
 
+// UC 3
+{
+    let pinCodeRegex = RegExp('^[0-9]{6}$');
+    arguments.forEach(element => {
+        console.log(element + " :", pinCodeRegex.test(element));
+    });
+}
+
+// UC 4
+let pinCodeRegex = RegExp('^[0-9]{3}\\s{0,1}[0-9]{3}$');
 arguments.forEach(element => {
     console.log(element + " :", pinCodeRegex.test(element));
 });
